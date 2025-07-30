c1931b619c8dcc1f41df68513a76237e6238ec8a
DOUMBA Arsel <amakita124@gmail.com>
2025-07-30 11:49:34 +0100

    V1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+console.log("Bienvenue sur le portfolio d'Axel Chancias!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
